8804bb18269a2201e5ae81a6e30cac6d155a513f
Wayne Kim <sumel014@naver.com>
2015-04-08 21:52:47 +0900

    언어 한글화 프로토타입 완료

--- qa-include/lang/qa-lang-emails.php
+++ qa-include/lang/qa-lang-emails.php
@@ -21,68 +21,68 @@
 */
 
 	return array(
-		'a_commented_body' => "^site_title のあなたの回答に^c_handleさんからコメントが投稿されました。\n\n^open^c_content^close\n\nあなたの回答:\n\n^open^c_context^close\n\nコメントする場合は下をクリック:\n\n^url\n\n^site_title",
-		'a_commented_subject' => '【^site_title】回答にコメントが投稿されました。',
+		'a_commented_body' => "^site_title 당신의 답변에^c_handle님이 댓글을 달았습니다.\n\n^open^c_content^close\n\n당신의 답변 :\n\n^open^c_context^close\n\n코멘트하려면 아래를 클릭:\n\n^url\n\n^site_title",
+		'a_commented_subject' => '【^site_title】답변에 댓글이 달리면되었습니다.。',
 
-		'a_followed_body' => "^site_title のあなたの回答に^q_handleさんから関連する質問が投稿されました。\n\n^open^q_title^close\n\nあなたの回答:\n\n^open^a_content^close\n\n質問に回答する場合は下をクリック:\n\n^url\n\n^site_title",
-		'a_followed_subject' => '【^site_title】関連質問が投稿されました。',
+		'a_followed_body' => "^site_title 당신의 답변에^q_handle님이 관련 질문이 게시했습니다.\n\n^open^q_title^close\n\n당신의 답변 :\n\n^open^a_content^close\n\n질문에 답변하는 경우는 아래를 클릭:\n\n^url\n\n^site_title",
+		'a_followed_subject' => '【^site_title】관련 질문이 게시되었습니다。',
 
-		'a_selected_body' => "おめでとうございます！^site_title の下の回答が^s_handleさんからベストアンサーに選ばれました。\n\n^open^a_content^close\n\n質問:\n\n^open^q_title^close\n\nあなたの回答は下をクリック:\n\n^url\n\n^site_title",
-		'a_selected_subject' => '【^site_title】回答がベストアンサーに選ばれました！',
+		'a_selected_body' => "축하합니다！^site_title 아래의 답변^s_handle이 베스트 답변으로 선택되었습니다.\n\n^open^a_content^close\n\n질문:\n\n^open^q_title^close\n\n당신의 답변은 아래를 클릭:\n\n^url\n\n^site_title",
+		'a_selected_subject' => '【^site_title】답변이 우수 답변에 선정되었습니다!',
 
-		'c_commented_body' => "^site_title のあなたのコメントの後に^c_handleさんからコメントが追加されました。\n\n^open^c_content^close\n\nコメント対象の発言：\n\n^open^c_context^close\n\nコメントする場合は下をクリック:\n\n^url\n\n^site_title",
-		'c_commented_subject' => '【^site_title】コメントが追加されました。',
+		'c_commented_body' => "^site_title 당신의 코멘트 후^c_handle님으로부터 코멘트가 추가되었습니다.\n\n^open^c_content^close\n\n코멘트 상대의 발언 :\n\n^open^c_context^close\n\n코멘트하려면 아래를 클릭:\n\n^url\n\n^site_title",
+		'c_commented_subject' => '【^site_title】코멘트가 추가되었습니다。',
 
-		'confirm_body' => "下のリンクをクリックして^site_titleのあなたのメールアドレスを認証してください。\n\n^url\n\n^site_title",
-		'confirm_subject' => '【^site_title】メールアドレスの確認',
+		'confirm_body' => "아래 링크를 클릭하여^site_title당신의 이메일 주소를 인증하십시오.\n\n^url\n\n^site_title",
+		'confirm_subject' => '【^site_title】이메일 주소 확인',
 
-		'feedback_body' => "コメント:\n^message\n\n名前:\n^name\n\nメールアドレス:\n^email\n\n直前のページ:\n^previous\n\nユーザー:\n^url\n\nIPアドレス:\n^ip\n\nブラウザ:\n^browser",
-		'feedback_subject' => '【^】問い合わせ',
+		'feedback_body' => "コメント:\n^message\n\n名前:\n^name\n\n이메일 주소 :\n^email\n\n이전 페이지 :\n^previous\n\n사용자 :\n^url\n\nIP 주소 :\n^ip\n\n브라우저 :\n^browser",
+		'feedback_subject' => '【^】문의',
 
-		'flagged_body' => "^p_handle さんの記事に ^flags がありました。\n\n^open^p_context^close\n\n記事は下をクリック:\n\n^url\n\n\n通報されたすべての記事は下をクリック:\n\n^a_url^site_title",
-		'flagged_subject' => '【^site_title】通報がありました。',
+		'flagged_body' => "^p_handle 님의 게시물에 ^flags 이있었습니다.\n\n^open^p_context^close\n\n게시물은 아래를 클릭 :\n\n^url\n\n\n통보 된 모든 게시물는 아래를 클릭 :\n\n^a_url^site_title",
+		'flagged_subject' => '【^site_title】통보가있었습니다.',
 
-		'moderate_body' => "^p_handle さんの記事を承認してください。\n\n^open^p_context^close\n\n記事の承認／否認は下をクリック:\n\n^url\n\n\n承認待ちの記事の一覧は下をクリック:\n\n^a_url\n\n^site_title",
-		'moderate_subject' => '【^site_title】記事を承認してください。',
+		'moderate_body' => "^p_handle 님의 기사를 승인하십시오.\n\n^open^p_context^close\n\n게시물의 승인 / 거부는 아래를 클릭 :\n\n^url\n\n\n승인 대기 게시물 목록은 아래를 클릭:\n\n^a_url\n\n^site_title",
+		'moderate_subject' => '【^site_title】게시물을 승인하십시오.',
 
-		'new_password_body' => "^site_title のあなたの新しいパスワードをお知らせします。\n\nパスワード: ^password\n\nログイン後すぐにこのパスワードを変更することをお勧めします。\n\n^site_title\n^url",
-		'new_password_subject' => '【^site_title】パスワード変更のお知らせ',
+		'new_password_body' => "^site_title 당신의 새 비밀번호를 알려드립니다。\n\n비밀번호: ^password\n\n로그인 후 즉시 암호를 변경하는 것이 좋습니다.\n\n^site_title\n^url",
+		'new_password_subject' => '【^site_title】비밀번호 변경 안내',
 
-		'private_message_body' => "^site_titleの^f_handleさんから非公開メッセージを受信しました。\n\n^open^message^close\n\n^more\n\n^site_title\n\nマイアカウントで非公開メッセージをブロックできます。\n^a_url",
-		'private_message_info' => "^f_handleさんの情報\n\n^url\n\n",
-		'private_message_reply' => "^f_handleさんへの返信は下をクリック\n\n^url",
-		'private_message_subject' => '【^site_title】^f_handleさんから非公開メッセージを受信',
+		'private_message_body' => "^site_titleの^f_handle님에게 비공개 메시지를 받았습니다.\n\n^open^message^close\n\n^more\n\n^site_title\n\n내 계정에 비공개 메시지를 차단할 수 있습니다.\n^a_url",
+		'private_message_info' => "^f_handle님의 정보\n\n^url\n\n",
+		'private_message_reply' => "^f_handle님에 대한 회신은 아래를 클릭\n\n^url",
+		'private_message_subject' => '【^site_title】^f_handle님에게 비공개 메시지를 수신',
 
-		'q_answered_body' => "^site_title のあなたの質問に^a_handleさんから回答が投稿されました。\n\n^open^a_content^close\n\nあなたの質問:\n\n^open^q_title^close\n\n回答は下をクリック:\n\n^url\n\n^site_title",
-		'q_answered_subject' => '【^site_title】質問に回答が投稿されました。',
+		'q_answered_body' => "^site_title 당신의 질문에^a_handle님 답변이 게시되었습니다.\n\n^open^a_content^close\n\n당신의 질문:\n\n^open^q_title^close\n\n답변은 아래를 클릭 :\n\n^url\n\n^site_title",
+		'q_answered_subject' => '【^site_title】질문에 대한 답변이 게시되었습니다。',
 
-		'q_commented_body' => "^site_title のあなたの質問に^c_handleさんからコメントが投稿されました。\n\n^open^c_content^close\n\nあなたの質問:\n\n^open^c_context^close\n\nコメントする場合は下をクリック:\n\n^url\n\n^site_title",
-		'q_commented_subject' => '【^site_title】質問にコメントが投稿されました。',
+		'q_commented_body' => "^site_title 당신의 질문에^c_handle님 댓글을 달았습니다.\n\n^open^c_content^close\n\n당신의 질문:\n\n^open^c_context^close\n\n코멘트하려면 아래를 클릭:\n\n^url\n\n^site_title",
+		'q_commented_subject' => '【^site_title】질문에 댓글이 달렸습니다',
 
-		'q_posted_body' => "^site_title に^q_handleさんから質問が投稿されました。\n\n^open^q_title\n\n^q_content^close\n\n質問は下をクリック:\n\n^url\n\n^site_title",
-		'q_posted_subject' => '【^site_title】質問が投稿されました。',
+		'q_posted_body' => "^site_title 에^q_handle님 질문이 게시되었습니다.\n\n^open^q_title\n\n^q_content^close\n\n질문은 아래를 클릭 :\n\n^url\n\n^site_title",
+		'q_posted_subject' => '【^site_title】질문 게시되었습니다.',
 
-		'remoderate_body' => "^p_handleさんが編集した記事を承認してください:\n\n^open^p_context^close\n\n編集された記事の承認／非表示は下をクリック:\n\n^url\n\n承認待ちの記事は下をクリック:\n\n^a_url\n\n\n^site_title",
-		'remoderate_subject' => '【^site_title】再承認通知',
+		'remoderate_body' => "^p_handle님이 편집한 게시물을 승인하십시오:\n\n^open^p_context^close\n\n편집된 게시물의 승인／숨기기은 아래를 클릭:\n\n^url\n\n승인 대기 게시물은 아래를 클릭:\n\n^a_url\n\n\n^site_title",
+		'remoderate_subject' => '【^site_title】승인알림',
 		
-		'reset_body' => "^site_title のあなたのパスワードをリセットする場合は下をクリック:\n\n^url\n\nまたは、ページ内のフィールドに下のコードを入力:\n\nコード: ^code\n\nパスワードをリセットしない場合は、このメールは無視してください。\n\n^site_title",
-		'reset_subject' => '【^site_title】パスワードリセットのお知らせ',
+		'reset_body' => "^site_title 당신의 비밀번호를 재설정하려면 아래를 클릭:\n\n^url\n\n또는 페이지의 필드에 아래의 코드를 입력:\n\n코드: ^code\n\n암호를 재설정하지 않으면이 메일은 무시하십시오.\n\n^site_title",
+		'reset_subject' => '【^site_title】암호 재설정 안내',
 
-		'to_handle_prefix' => "^ さん\n\n",
+		'to_handle_prefix' => "^ 님\n\n",
 
-		'u_registered_body' => "^u_handleさんが新しいユーザーとして登録されました。\n\nプロフィールは下をクリック:\n\n^url\n\n^site_title",
-		'u_to_approve_body' => "^u_handleさんが新しいユーザーとして登録されました。\n\nユーザーの承認は下をクリック:\n\n^url\n\n承認待ちのユーザーは下をクリック:\n\n^a_url\n\n^site_title",
-		'u_registered_subject' => '【^site_title】新しいユーザーが登録されました。',
+		'u_registered_body' => "^u_handle님 새로운 사용자로 등록되었습니다.\n\n프로필은 아래를 클릭:\n\n^url\n\n^site_title",
+		'u_to_approve_body' => "^u_handle님 새로운 사용자로 등록되었습니다.\n\n사용자의 승인 아래를 클릭:\n\n^url\n\n승인 대기중인 사용자는 아래를 클릭:\n\n^a_url\n\n^site_title",
+		'u_registered_subject' => '【^site_title】새로운 사용자가 등록되었습니다.',
 		
-		'u_approved_body' => "プロフィールは下をクリック:\n\n^url\n\n^site_title",
-		'u_approved_subject' => '【^site_title】あなたのアカウントが承認されました。',
+		'u_approved_body' => "프로필은 아래를 클릭:\n\n^url\n\n^site_title",
+		'u_approved_subject' => '【^site_title】당신의 계정이 승인되었습니다。',
 		
-		'wall_post_subject' => '【^site_title】公開メッセージ',
-		'wall_post_body' => "^f_handleさんが^site_titleのあなたの公開メッセージに投稿しました。\n\n^open^post^close\n\n返事はこちら:\n\n^url\n\n^site_title",
+		'wall_post_subject' => '【^site_title】공개 메시지',
+		'wall_post_body' => "^f_handle님이 ^site_title당신의 공개 메시지를 게시했습니다.\n\n^open^post^close\n\n대답은 여기:\n\n^url\n\n^site_title",
 
-		'welcome_body' => "^site_title のユーザー登録ありがとうございます。\n\n^custom^confirmログイン情報:\n\nユーザー名: ^handle\n\nメールアドレス: ^email\n後で参照できるように、この情報は大切に保管してください。\n\n^site_title\n^url",
-		'welcome_confirm' => "下のリンクをクリックしてあなたのメールアドレスを認証してください。\n\n^url\n\n",
-		'welcome_subject' => '【^site_title】ユーザー登録通知',
+		'welcome_body' => "^site_title 사용자 등록 감사합니다.\n\n^custom^confirm로그인 정보:\n\n사용자 이름: ^handle\n\n이메일 주소: ^email\n나중에 참조 할 수 있도록이 정보를 잘 보관하십시오.\n\n^site_title\n^url",
+		'welcome_confirm' => "아래 링크를 클릭하여 귀하의 이메일 주소를 인증하십시오。\n\n^url\n\n",
+		'welcome_subject' => '【^site_title】등록 알림',
 	);
 	
 
